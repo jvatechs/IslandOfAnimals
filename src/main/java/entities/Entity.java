4b0af7bb150bee91e25549868351e0874fe83aad
Anar Mamedov <68174767+jvatechs@users.noreply.github.com>
2023-04-18 09:26:27 +0400

    finished under implementing eat method which deeply related with locations. It made for decreasing animals or plants after was eaten by other animal.

--- src/main/java/entities/Entity.java
+++ src/main/java/entities/Entity.java
@@ -1,9 +1,13 @@
 package entities;
 
+import island.CreateIsland;
+import island.Location;
+
 public abstract class Entity {
     public boolean canEatAnimalsOrNot() {
         return EatOtherAnimalAble.class.isAssignableFrom(this.getClass());
     }
 
+    Location[][] locations = CreateIsland.island;
 
 }
